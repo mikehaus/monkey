c6bcccd8d0e43199e6fca3416d041cae5a73a111
Mike Hollingshaus <mike.haus.dev@gmail.com>
2025-06-14 08:08:52 -0600

    Handle index expressions

--- parser/parser.go
+++ parser/parser.go
@@ -35,6 +35,7 @@ const (
 	PRODUCT     // *
 	PREFIX      // -x or !x
 	CALL        // myFunction(x)
+	INDEX       // array[index]
 )
 
 var precedences = map[token.TokenType]int{
@@ -47,6 +48,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -79,6 +81,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	return p
 }
@@ -202,7 +205,7 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 
 	if p.peekTokenIs(end) {
 		p.nextToken()
-		return list 
+		return list
 	}
 
 	p.nextToken()
@@ -436,6 +439,19 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	exp := &ast.CallExpresion{Token: p.curToken, Function: function}
 	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+
 	return exp
 }
 
